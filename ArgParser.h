2666851efbf79819c98ea6ccc523248b962d6ded
Deepesh Choudhary <dcdeepesh7@gmail.com>
2019-07-26 20:30:50 +0530

    Document ArgParser header

--- ArgParser.h
+++ ArgParser.h
@@ -5,25 +5,120 @@
 #include <vector>
 #include <utility>  //for std::pair
 
+/**
+ * Parses the arguments given in initialization process to
+ * seperate different types of options(switches, data options),
+ * associate data to the data options etc.
+ * Almost equivalent to getopt() in *nix.
+ */
 class ArgParser {
 public:
+    /**
+     * Creates an empty ArgParser. operator() should be called
+     * on these empty-initialized objects before parsing them.
+     */
     ArgParser();
-    ArgParser(const int argc, const char *argv[],
-        const std::string switches, const std::string dataOptions = "");
-    void operator()(const int argc, const char *argv[],
-        const std::string switches, const std::string dataOptions = "");
 
+    /**
+     * Creates the object with all required data.
+     *
+     * @param argc argc from main()
+     * @param argv argv from main()
+     * @param switches A string containg all single-letter switches
+     * @param dataOptions A string containg all single-letter data options
+     *
+     * e.g. ArgParser(argc, argv, "cgfTR", "hyIPo");
+     */
+    ArgParser(const int argc, const char *argv[],
+        const std::string switches, const std::string dataOptions);
+
+    /**
+     * Assigns required data to class members. Should be called if the
+     * object was inilitally empty-initialized; no need otherwise.
+     *
+     * @param argc argc from main()
+     * @param argv argv from main()
+     * @param switches A string containg all single-letter switches
+     * @param dataOptions A string containg all single-letter data options
+     *
+     * e.g. ArgParser ap; //empty-initialize
+     *      ap(argc, argv, "cgfTR", "hyIPo");
+     */
+    void operator()(const int argc, const char *argv[],
+        const std::string switches, const std::string dataOptions);
+
+    /**
+     * Parses the arguments and stores results in the object.
+     * Can be called only after providing required data via
+     * constructor or operator().
+     *
+     * @throws ArgParseException If the required data was not provided earlier.
+     */
     void parse();
 
+    /**
+     * Checks if the specified switch is set or not. The switch should NOT
+     * be prefixed by '-' or '--'.
+     *
+     * @param svitch Switch to check
+     * @return true if set false otherwise
+     */
     bool isSwitchSet(std::string svitch) const;
+
+    /**
+     * Checks if the specified data option is set or not. The data option
+     * should NOT be prefixed by '-' or '--'.
+     *
+     * @param dataOption Option to check
+     * @return true if set false otherwise
+     */
     bool isDataOptSet(std::string dataOption) const;
+
+    /**
+     * Checks if the specified option is set or not. Option can be switch
+     * ot data option. The option should NOT be prefixed by '-' or '--'.
+     *
+     * @param option Option to check
+     * @return true if set false otherwise
+     */
     bool isSet(std::string option) const;
 
+    /**
+     * Returns the data provided with specified data option.
+     *
+     * @param option Data option to check for data
+     * @return Data provided with the option
+     * @throws ArgParseException If the specified option is not a data option.
+     */
     std::string getDataForOpt(std::string option) const;
     
+    /**
+     * Returns a vector containing all single- and multi-letter switches.
+     *
+     * @return vector of all switches
+     */
     std::vector<std::string> getSwitches() const;
+
+    /**
+     * Returns a vector containing all single- and multi-letter data options.
+     *
+     * @return vector of all data options
+     */
     std::vector<std::string> getDataOptions() const;
+
+    /**
+     * Returns a vector containing all single- and multi-letter options.
+     *
+     * @return vector of all options
+     */
     std::vector<std::string> getAllOptions() const;
+
+    /**
+     * Returns a vector containing all operands. Operands are arguments
+     * that are neither options nor data to any options.
+     *
+     * @return vector of all operands
+     */
     std::vector<std::string> getOperands() const;
 
 private:
