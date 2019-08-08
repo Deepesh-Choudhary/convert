49c766ac52fae36ead0162e1a9d27e1751cd5860
Deepesh Choudhary <dcdeepesh7@gmail.com>
2019-08-08 17:55:06 +0530

    Fix conversions.cpp

--- conversions.cpp
+++ conversions.cpp
@@ -54,33 +54,33 @@ string BaseConversions::fromBase10(const string &str, const int toBase) {
     return result;
 }
 
-inline string BaseConversions::changeBase(const string &str, const int fromBase, const int toBase) {
+string BaseConversions::changeBase(const string &str, const int fromBase, const int toBase) {
     return fromBase10(toBase10(str, fromBase), toBase);
 }
 
 //use dictionary instead?
-inline string bin2hex(const string &bin) {
+string BaseConversions::bin2hex(const string &bin) {
     return BaseConversions::changeBase(bin, 2, 16);
 }
 
-inline string bin2dec(const string &bin) {
+string BaseConversions::bin2dec(const string &bin) {
     return BaseConversions::changeBase(bin, 2, 10);
 }
 
-inline string dec2bin(const string &dec) {
+string BaseConversions::dec2bin(const string &dec) {
     return BaseConversions::changeBase(dec, 10, 2);
 }
 
-inline string dec2hex(const string &dec) {
+string BaseConversions::dec2hex(const string &dec) {
     return BaseConversions::changeBase(dec, 10, 16);
 }
 
 //use dictionary instead?
-inline string hex2bin(const string &hex) {
+string BaseConversions::hex2bin(const string &hex) {
     return BaseConversions::changeBase(hex, 16, 2);
 }
 
-inline string hex2dec(const string &hex) {
+string BaseConversions::hex2dec(const string &hex) {
     return BaseConversions::changeBase(hex, 16, 10);
 }
 
