448091cb54ba2a60798a2d3752f47ebd4f104df8
Deepesh Choudhary <dcdeepesh7@gmail.com>
2019-08-11 12:03:14 +0530

    Document and refactor conversionsCore

--- conversionsCore.cpp
+++ conversionsCore.cpp
@@ -7,14 +7,23 @@
 
 using std::string;
 
+/**
+ * Algorithm: Raise the base to successive powers and multiply
+ * the result by numbers starting sequentially from right side.
+ */
 string toBase10Core(const string &str, const int fromBase) {
     int power = 0, result = 0;
     for(auto iter = str.rbegin(); iter != str.rend(); ++iter)
-        result += valueOf(*iter) * pow(fromBase, power++);
+        result += valueOf(*iter) * std::pow(fromBase, power++);
 
     return std::to_string(result);
 }
 
+/**
+ * Algorithm: Divide the number by base and store the remainders
+ * in reverse order. Remainders are represented by digits and
+ * alphabets.
+ */
 string fromBase10Core(const string &str, const int toBase) {
     string result;
     int num = std::stoi(str);
--- conversionsCore.h
+++ conversionsCore.h
@@ -3,7 +3,20 @@
 
 #include <string>
 
+/**
+ * Core function for BaseConversions::toBase10().
+ * CAUTION: This function is not meant to be used directly by the users.
+ * Any usage with wrong/unexpected input may cause your code to break.
+ * Use BaseConversions::toBase10() instead (see conversions.h).
+ */
 std::string toBase10Core(const std::string &str, const int fromBase);
+
+/**
+ * Core function for BaseConversions::fromBase10().
+ * CAUTION: This function is not meant to be used directly by the users.
+ * Any usage with wrong/unexpected input may cause your code to break.
+ * Use BaseConversions::fromBase10() instead (see conversions.h).
+ */
 std::string fromBase10Core(const std::string &str, const int toBase);
 
 #endif
